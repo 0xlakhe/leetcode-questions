aeba608e196b04c89c2c2a0f96950ca83dd3331c
0xlakhe <249052680+0xlakhe@users.noreply.github.com>
2026-04-30 09:46:56 +0545

    Time: 6 ms (69.45%), Space: 46.7 MB (20.16%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,21 +1,40 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int longest=Integer.MIN_VALUE;
+        //brute force
+        // int longest=Integer.MIN_VALUE;
+        // for(int i=0;i<s.length();i++){
+        //     HashSet<Character> characters=new HashSet<>();
+        //     int count=0;
+        //     for(int j=i;j<s.length();j++){
+        //         if(characters.contains(s.charAt(j))){
+        //             break;
+        //         }
+        //         characters.add(s.charAt(j));
+        //         count+=1;
+        //     }
+        //     if(count>longest){
+        //         longest=count;
+        //     }
+        // }
+        
+        // return (longest==Integer.MIN_VALUE)?0:longest;
+                int longest=Integer.MIN_VALUE;
+        HashSet<Character> characters=new HashSet<>();
+        int j=0;
+
+
         for(int i=0;i<s.length();i++){
-            HashSet<Character> characters=new HashSet<>();
-            int count=0;
-            for(int j=i;j<s.length();j++){
-                if(characters.contains(s.charAt(j))){
-                    break;
-                }
-                characters.add(s.charAt(j));
-                count+=1;
+            
+            while (characters.contains(s.charAt(i))) {
+                characters.remove(s.charAt(j));
+                j++;
             }
-            if(count>longest){
-                longest=count;
+            characters.add(s.charAt(i));
+            if(characters.size()>longest){
+                longest=characters.size();
             }
         }
-        
+
         return (longest==Integer.MIN_VALUE)?0:longest;
     }
 }
