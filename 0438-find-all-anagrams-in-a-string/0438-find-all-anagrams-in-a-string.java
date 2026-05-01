66f20e84f135587045265bc10f8a3ca272da36c6
0xlakhe <249052680+0xlakhe@users.noreply.github.com>
2026-05-01 09:57:45 +0545

    Time: 10 ms (83.33%), Space: 47 MB (46.47%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,40 +1,32 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
+        int[] sFreq=new int[26];
+        int[] pFreq=new int[26];
         int index=0;
-        boolean check=false;
-        ArrayList<Integer> points=new ArrayList<>();
+        ArrayList<Integer> indexes=new ArrayList<>();
 
         if(p.length()>s.length()){
-            return points;
+            return indexes;
         }
-        int[] characters=new int[26];
 
-        for(Character character:p.toCharArray()){
-            characters[character-'a']+=1;
-        } 
-
-        int[] toCompare=new int[26];
         for(int i=0;i<p.length();i++){
-            Character letter=s.charAt(i);
-            toCompare[letter-'a']+=1;
+            sFreq[s.charAt(i)-'a']++;
+            pFreq[p.charAt(i)-'a']++;
         }
 
+        if(Arrays.equals(sFreq, pFreq)){
+            indexes.add(0);
+        }
 
-        
-        for(int j=p.length();j<=s.length();j++){
-            if(Arrays.equals(characters, toCompare)){
-                points.add(index);
-            }
-            Character toRemove=s.charAt(index);
-            toCompare[toRemove-'a']-=1;
+        for(int j=p.length();j<s.length();j++){
+            sFreq[s.charAt(j)-'a']++;
+            sFreq[s.charAt(index)-'a']--;
             index++;
-            if(j<s.length()){
-                Character toAdd=s.charAt(j);
-                toCompare[toAdd-'a']+=1;
-            }else{
-                break;
+
+            if(Arrays.equals(sFreq, pFreq)){
+                indexes.add(index);
             }
         }
-        return points;
+        return indexes;
     }
 }
