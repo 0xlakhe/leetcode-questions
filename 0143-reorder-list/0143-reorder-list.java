ff7bd5db6e381e20cb9373bdd4364f9a9aecc19d
0xlakhe <249052680+0xlakhe@users.noreply.github.com>
2026-05-04 20:48:22 +0545

    Time: 2 ms (88.46%), Space: 48.6 MB (99.29%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -10,6 +10,31 @@
  */
 class Solution {
     public void reorderList(ListNode head) {
+                // if (head == null || head.next == null || head.next.next == null) {
+        //     return;
+        // }
+
+        // ListNode dummy=new ListNode(0);
+        // ListNode tail=dummy;
+        // ListNode current=head;
+        // tail.next=head;
+
+        // while(current!=null && current.next!=null && current.next.next!=null){
+        //     ListNode runner=current;
+        //     current=current.next;
+        //     while (runner.next!=null){
+        //         if(runner.next.next==null){
+        //             tail.next=runner.next;
+        //             tail=tail.next;
+        //             runner.next=null;
+        //             break;
+        //         }
+        //         runner=runner.next;
+        //     }
+        //     tail.next=current;
+        //     tail=tail.next;
+        // }
+        // head=dummy.next;
         if(head==null || head.next==null){
             return;
         }
