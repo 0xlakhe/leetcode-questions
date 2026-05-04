1e4afa48800b2e3bb6337711080bb11a7a613b47
0xlakhe <249052680+0xlakhe@users.noreply.github.com>
2026-05-04 20:45:47 +0545

    Time: 2 ms (88.46%), Space: 48.5 MB (99.29%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -10,30 +10,38 @@
  */
 class Solution {
     public void reorderList(ListNode head) {
-        ListNode dummy=new ListNode(0);
-
-        ListNode tail=dummy;
-        ListNode current=head;
-        tail.next=head;
-        if (head == null || head.next == null || head.next.next == null) {
-        return;
-    }
-        while(current!=null && current.next!=null && current.next.next!=null){
-            ListNode runner=current;
-            current=current.next;
-            while (runner.next!=null){
-                if(runner.next.next==null){
-                    tail.next=runner.next;
-                    tail=tail.next;
-                    runner.next=null;
-                    break;
-                }
-                runner=runner.next;
-            }
-            tail.next=current;
-            tail=tail.next;
+        if(head==null || head.next==null){
+            return;
         }
-        head.next=dummy.next;
+
+        ListNode slow=head;
+        ListNode fast=head;
+
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+
+        ListNode prev=null;
+        ListNode current=slow.next;
+        slow.next=null;
+        while(current!=null){
+            ListNode next=current.next;
+            current.next=prev;
+            prev=current;
+            current=next;
+        }
+
+        ListNode start=head;
+        ListNode end=prev;
+        while(end!=null){
+            ListNode tempStart=start.next;
+            ListNode tempPrev=end.next;
+            start.next=end;
+            end.next=tempStart;
+            start=tempStart;
+            end=tempPrev;
+        }
 
     }
 }
