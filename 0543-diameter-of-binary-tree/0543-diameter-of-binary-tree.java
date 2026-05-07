fced9e51165abb7b239a3de68051cea52d09da54
0xlakhe <249052680+0xlakhe@users.noreply.github.com>
2026-05-07 07:21:58 +0545

    Time: 1 ms (42.14%), Space: 46.7 MB (95.71%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -17,7 +17,6 @@ class Solution {
     int max=0;
 
     public int diameterOfBinaryTree(TreeNode root) {
-
         maxDepth(root);
         return max;
     }
@@ -26,12 +25,9 @@ class Solution {
         if(root==null){
             return 0;
         }
-
         int left=maxDepth(root.left);
         int right=maxDepth(root.right);
-        if(left+right>max){
-            max=left+right;
-        }
+        max= Math.max(max,left+right);
         return 1+ Math.max(left,right);
     }
 }
