fa1e7328bbf86f02503cf9e3dcd41587e17bbc61
0xlakhe <249052680+0xlakhe@users.noreply.github.com>
2026-05-09 10:36:30 +0545

    Time: 0 ms (100%), Space: 42.4 MB (17.05%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,13 @@
+class Solution {
+    HashMap<Integer,Integer> map=new HashMap<>();
+    public int climbStairs(int n) {
+        if(n==1||n==2){
+            return n;
+        }
+        if(map.containsKey(n)){
+            return map.get(n);
+        }
+        map.put(n,climbStairs(n-1)+climbStairs(n-2));
+        return map.get(n);
+    }
+}
