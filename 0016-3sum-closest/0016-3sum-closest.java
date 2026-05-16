e337b018b9e5fb004d66a783ffe864fac816ffc9
0xlakhe <249052680+0xlakhe@users.noreply.github.com>
2026-05-16 20:34:19 +0545

    Time: 18 ms (41.51%), Space: 45.5 MB (65.94%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,17 +1,38 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
         //brute force;
-        int ans= Integer.MAX_VALUE;
+        // int ans= Integer.MAX_VALUE;
+        // int result=0;
+        // for(int i=0;i<nums.length;i++){
+        //     for(int j=i+1;j<nums.length;j++){
+        //         for(int k=j+1;k<nums.length;k++){
+        //             int sum=nums[i]+nums[j]+nums[k];
+        //             int diff=Math.abs(target-sum);
+        //             if(diff<ans){
+        //                 ans=diff;
+        //                 result=sum;
+        //             } 
+        //         }
+        //     }
+        // }
+        // return result;
+        int ans=Integer.MAX_VALUE;
         int result=0;
+        Arrays.sort(nums);
         for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                for(int k=j+1;k<nums.length;k++){
-                    int sum=nums[i]+nums[j]+nums[k];
-                    int diff=Math.abs(target-sum);
-                    if(diff<ans){
-                        ans=diff;
-                        result=sum;
-                    }   
+            int j=i+1;
+            int k=nums.length-1;
+            while(j<k){
+                int sum= nums[i]+nums[j]+nums[k];   
+                int diff= Math.abs(sum-target);
+                if(diff<ans){
+                    ans=diff;
+                    result=sum;
+                }
+                if(sum>target){
+                    k-=1;
+                }else{
+                    j+=1;
                 }
             }
         }
