a79089e1f850f4a383786fe5fe1aed74c6106988
0xlakhe <249052680+0xlakhe@users.noreply.github.com>
2026-05-17 07:47:59 +0545

    Time: 19 ms (80.92%), Space: 45.7 MB (76.31%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -3,19 +3,28 @@ class Solution {
         List<List<Integer>> res=new ArrayList<>();
         Arrays.sort(nums);
         for(int i=0;i<nums.length;i++){
+            if(i!=0 && nums[i]==nums[i-1]){
+                continue;
+            }
             for(int j=i+1;j<nums.length;j++){
+                if(j!=i+1 && nums[j]==nums[j-1]){
+                    continue;
+                }
                 int x=j+1;
                 int y=nums.length-1;
                 while(x<y){
                     long sum=(long)nums[i]+nums[j]+nums[x]+nums[y];
                     if(sum==target){
                         List<Integer> list= Arrays.asList(nums[i],nums[j],nums[x],nums[y]);
-                        Collections.sort(list);
-                        if(!res.contains(list)){
-                            res.add(list);
-                        }
-                        y--;
+                        res.add(list);
                         x++;
+                        y--;
+                        while(x<y && nums[x]==nums[x-1]){
+                            x++;
+                        }
+                        while(x<y && nums[y]==nums[y+1]){
+                            y--;
+                        }
                     }else if(sum>target){
                         y--;
                     }else{
