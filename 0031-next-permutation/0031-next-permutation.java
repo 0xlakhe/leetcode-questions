a770e9a1c752b78a92d1efe9739415c17c99b20c
0xlakhe <249052680+0xlakhe@users.noreply.github.com>
2026-05-18 15:09:55 +0545

    Time: 2 ms (8.13%), Space: 45 MB (15.08%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,64 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        // int right=nums.length-1;
+        // int left=right-1;
+        // if(nums.length==1){
+        //     return;
+        // }
+        // if(nums[right]>nums[left]){
+        //     int temp=nums[left];
+        //     nums[left]=nums[right];
+        //     nums[right]=temp;
+        //     return;
+        // }
+        // if(nums[0]>=nums[left] && nums[0]>=nums[right]){
+        //     Arrays.sort(nums);
+        //     return;
+        // }
+
+        // int temp=nums[0];
+        // if(nums[0]>=nums[right] && nums[left]>nums[right]){
+        //     nums[0]=nums[left];
+        //     nums[left]=nums[right];
+        //     nums[right]=temp;
+        // }else{
+        //     nums[0]=nums[right];
+        //     nums[right]=nums[left];
+        //     nums[left]=temp;
+        // }   
+        boolean check=true;
+        for(int i=1; i<nums.length;i++){
+            if(nums[i]>nums[i-1]){
+                check=false;
+            }
+        }
+        if (check){
+            Arrays.sort(nums);
+            return;
+        }
+        int i=nums.length-1;
+        while(nums[i-1]>=nums[i]){
+            i--;
+        }
+        int j=i-1;
+        i=nums.length-1;
+        while(i>j){
+            if(nums[i]>nums[j]){
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                break;
+            }
+            i--;
+        }
+        i=nums.length-1;
+        j+=1;
+        while(i>j){
+            int temp = nums[j];
+            nums[j] = nums[i];
+            nums[i] = temp;
+            i--;
+            j++;
+        }
+    }
+}
