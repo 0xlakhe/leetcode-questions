053ead8c65da0d04b4433759e60d882a02ad69f4
0xlakhe <249052680+0xlakhe@users.noreply.github.com>
2026-05-23 10:33:12 +0545

    Time: 0 ms (100%), Space: 45.5 MB (31.91%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -10,22 +10,35 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        ListNode dummy=new ListNode(0);
-        dummy.next=head;
-        ListNode prev=dummy.next;
+        // ListNode dummy=new ListNode(0);
+        // dummy.next=head;
+        // ListNode prev=dummy.next;
 
-        while(prev!=null){
-            if(prev!=null && prev.next!=null && prev.val==prev.next.val){
-                int dup=prev.val;
-                ListNode current=prev;
-                while(current!=null && dup==current.val){
-                    current=current.next;
-                }
-                prev.next=current;
+        // while(prev!=null){
+        //     if(prev!=null && prev.next!=null && prev.val==prev.next.val){
+        //         int dup=prev.val;
+        //         ListNode current=prev;
+        //         while(current!=null && dup==current.val){
+        //             current=current.next;
+        //         }
+        //         prev.next=current;
+        //     }else{
+        //         prev=prev.next;
+        //     }
+        // }
+        // return dummy.next;
+
+        if(head==null || head.next==null){
+            return head;
+        }
+        ListNode current=head;
+        while(current.next!=null){
+            if(current.val==current.next.val){
+                current.next=current.next.next;
             }else{
-                prev=prev.next;
+                current=current.next;
             }
         }
-        return dummy.next;
+        return head;
     }
 }
