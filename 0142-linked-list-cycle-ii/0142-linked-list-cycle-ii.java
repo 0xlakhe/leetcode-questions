c82bb7cb074bb9f6e82281049ab2b554c9ebfb30
0xlakhe <249052680+0xlakhe@users.noreply.github.com>
2026-05-25 08:33:02 +0545

    Time: 0 ms (100%), Space: 46.8 MB (29.06%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -22,13 +22,9 @@ public class Solution {
         // }
         // return head;
 
-        if(head==null || head.next==null){
-            return null;
-        }
-
         ListNode slow=head;
         ListNode fast=head;
-        while(fast.next!=null && fast.next.next!=null){
+        while(fast!=null && fast.next!=null){
             slow=slow.next;
             fast=fast.next.next;
             if(slow==fast){
