b41ccf2d7868a7a8fb14c2b9189f382aac88a8ae
0xlakhe <249052680+0xlakhe@users.noreply.github.com>
2026-05-26 11:59:56 +0545

    Time: 9 ms (98.72%), Space: 59.2 MB (69.44%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -11,21 +11,69 @@
 class Solution {
 
     public ListNode sortList(ListNode head) {
-        ArrayList<Integer> list=new ArrayList<>();
-        ListNode current=head;
-        while(current!=null){
-            list.add(current.val);
-            current=current.next;
-        }
-        ListNode current1=head;
-        int i=0;
-        Collections.sort(list);
+        
+        //brute force
+        // ArrayList<Integer> list=new ArrayList<>();
+        // ListNode current=head;
+        // while(current!=null){
+        //     list.add(current.val);
+        //     current=current.next;
+        // }
+        // ListNode current1=head;
+        // int i=0;
+        // Collections.sort(list);
 
-        while(current1!=null && i<list.size()){
-            current1.val=list.get(i);
-            current1=current1.next;
-            i++;
+        // while(current1!=null && i<list.size()){
+        //     current1.val=list.get(i);
+        //     current1=current1.next;
+        //     i++;
+        // }
+        // return head;
+        if(head==null || head.next==null){
+            return head;
         }
-        return head;
+
+        ListNode mid=middle(head);
+        ListNode leftHead=head;
+        ListNode rightHead=mid.next;
+        mid.next=null;
+        
+        leftHead=sortList(leftHead);
+        rightHead=sortList(rightHead);
+
+        return merge(leftHead,rightHead);
+    }
+
+    public ListNode merge(ListNode leftHead, ListNode rightHead){
+        ListNode dummy=new ListNode(-1);
+        ListNode temp=dummy;
+        while(leftHead!=null && rightHead!=null){
+            if(leftHead.val<rightHead.val){
+                temp.next=leftHead;
+                temp=leftHead;
+                leftHead=leftHead.next;
+            }else{
+                temp.next=rightHead;
+                temp=rightHead;
+                rightHead=rightHead.next;
+            }
+        }
+        if(leftHead!=null){
+            temp.next=leftHead;
+        }
+        if(rightHead!=null){
+            temp.next=rightHead;
+        }
+        return dummy.next;
+    }
+
+    public ListNode middle(ListNode head){
+        ListNode slow=head;
+        ListNode fast=head.next;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
     }
 }
