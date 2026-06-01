8e7f7c7d6c05ac46c163a0276d95d3cab45a0154
0xlakhe <249052680+0xlakhe@users.noreply.github.com>
2026-06-01 16:05:20 +0545

    Time: 19 ms (18.36%), Space: 48.5 MB (8.03%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.java
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.java
@@ -1,23 +1,39 @@
 class Solution {
     public int findLHS(int[] nums) {
-        if (nums.length==0){
-            return 0;
+        //brute
+        // if (nums.length==0){
+        //     return 0;
+        // }
+        // Arrays.sort(nums);
+        // int right=1;
+        // int left=0;
+        // int count=0;
+        // while(right<nums.length){
+        //     int diff=nums[right]-nums[left];
+        //     if(diff>1){
+        //         left++;
+        //     }else {
+        //         if(diff==1){
+        //             count=Math.max(count, right-left+1);
+        //         }
+        //         right++;
+        //     }
+        // }
+        // return count;
+
+        HashMap<Integer,Integer> freq=  new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            freq.put(nums[i],freq.getOrDefault(nums[i], 0)+1);
         }
-        Arrays.sort(nums);
-        int right=1;
-        int left=0;
-        int count=0;
-        while(right<nums.length){
-            int diff=nums[right]-nums[left];
-            if(diff>1){
-                left++;
-            }else {
-                if(diff==1){
-                    count=Math.max(count, right-left+1);
+        int ans=0;
+        for(Integer key: freq.keySet()){
+            if(freq.containsKey(key+1)){
+                int length= freq.get(key)+ freq.get(key+1);
+                if(length>ans){
+                    ans=length;
                 }
-                right++;
             }
         }
-        return count;
+        return ans;
     }
 }
