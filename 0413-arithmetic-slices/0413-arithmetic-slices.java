5f6b044e654e625c353577969195f00e32375a34
0xlakhe <249052680+0xlakhe@users.noreply.github.com>
2026-06-01 08:44:36 +0545

    Time: 0 ms (100%), Space: 42.9 MB (89.27%) - LeetHub

---
+++ 0413-arithmetic-slices/0413-arithmetic-slices.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numberOfArithmeticSlices(int[] nums) {
+        if(nums.length<3){
+            return 0;
+        }
+        int ans=0;
+        int left=0;
+        while(left<nums.length-2){
+            int right=left+1;
+            int diff=nums[right]-nums[left];
+
+            while (right + 1 < nums.length && nums[right + 1] - nums[right] == diff) {
+                right++;
+            }
+            int len=right-left+1;
+
+            if(len>=3){
+                ans+=(len-1)*(len-2)/2;
+            }
+            left=right;
+        }
+        return ans;
+    }
+}
