36f5adafb0219ba58aba1b20d16baee0e19f4ef2
0xlakhe <249052680+0xlakhe@users.noreply.github.com>
2026-06-05 08:50:44 +0545

    Time: 1 ms (96.17%), Space: 42.9 MB (68.67%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
@@ -6,7 +6,6 @@ class Solution {
             return 0;
         }
         int ans=0;
-        boolean check=true;
         while(right<s.length()){
             if(s.charAt(left)!=s.charAt(left+1)&& s.charAt(left+1)!=s.charAt(right)&&s.charAt(left)!=s.charAt(right)){
                 ans+=1;
