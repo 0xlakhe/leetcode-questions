60b6fa5a9bbbb5b7b78f1428cb49c2a05e575409
0xlakhe <249052680+0xlakhe@users.noreply.github.com>
2026-06-05 08:50:32 +0545

    Time: 1 ms (96.17%), Space: 42.8 MB (68.67%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
@@ -1,42 +1,18 @@
 class Solution {
     public int countGoodSubstrings(String s) {
-        int[] map=new int[26];
         int left=0;
-        int right=0;
+        int right=2;
         if(s.length()<3){
             return 0;
         }
-        while(right<3){
-            char c=s.charAt(right);
-            map[c-'a']+=1;
-            right++;
-        }
         int ans=0;
         boolean check=true;
-        for(int i=0; i<26;i++){
-            if(map[i]>1){
-                check=false;
-            }
-        }
-        if(check==true){
-            ans+=1;
-        }
         while(right<s.length()){
-            char cRight=s.charAt(right);
-            char cLeft= s.charAt(left);
-            map[cLeft-'a']--;
-            map[cRight-'a']++;
-            right++;
-            left++;
-            boolean checker=true;
-            for(int j=0;j<26;j++){
-                if(map[j]>1){
-                    checker=false;
-                }
-            }
-            if(checker==true){
+            if(s.charAt(left)!=s.charAt(left+1)&& s.charAt(left+1)!=s.charAt(right)&&s.charAt(left)!=s.charAt(right)){
                 ans+=1;
             }
+            right++;
+            left++;
         }
         return ans;
     }
