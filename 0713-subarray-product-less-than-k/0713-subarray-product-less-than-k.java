7b5f65a50daebcc3d98c945c619a0be43d83d701
0xlakhe <249052680+0xlakhe@users.noreply.github.com>
2026-06-06 21:24:45 +0545

    Time: 4 ms (99.88%), Space: 51.4 MB (38.48%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
@@ -1,18 +1,34 @@
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
-        int ans=0;
-        ArrayList<ArrayList<Integer>> list=new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-            int mul=1;
-            for(int j=i;j<nums.length;j++){
-                mul*=nums[j];
-                if(mul<k){
-                    ans+=1;
-                }else{
-                    break;
-                }
+        // int ans=0;
+        // ArrayList<ArrayList<Integer>> list=new ArrayList<>();
+        // for(int i=0;i<nums.length;i++){
+        //     int mul=1;
+        //     for(int j=i;j<nums.length;j++){
+        //         mul*=nums[j];
+        //         if(mul<k){
+        //             ans+=1;
+        //         }else{
+        //             break;
+        //         }
 
+        //     }
+        // }
+        // return ans;
+
+        if(k<=1){
+            return 0;
+        }
+        int ans=0;
+        int mul=1;
+        int left=0;
+        for(int right=0; right<nums.length;right++){
+            mul*=nums[right];
+            while(mul>=k){
+                mul/=nums[left];
+                left++;
             }
+            ans+=(right-left+1);
         }
         return ans;
     }
