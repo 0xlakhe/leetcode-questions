94f92e214c4f9dc4cf425a045a002fe5a46903e9
0xlakhe <249052680+0xlakhe@users.noreply.github.com>
2026-06-12 09:05:57 +0545

    Time: 0 ms (100%), Space: 42.4 MB (96.17%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,27 +1,55 @@
 class Solution {
     public String decodeString(String s) {
-        Deque<Integer> countStack=new ArrayDeque<>();
-        Deque<String> stringStack= new ArrayDeque<>();
-        String currentString="";
-        int currentNumber=0;
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
+        // brute force
+        // Deque<Integer> countStack=new ArrayDeque<>();
+        // Deque<String> stringStack= new ArrayDeque<>();
+        // String currentString="";
+        // int currentNumber=0;
+        // for(int i=0;i<s.length();i++){
+        //     char ch=s.charAt(i);
+        //     if(Character.isDigit(ch)){
+        //         currentNumber=currentNumber*10+(ch-'0');
+        //     }else if(Character.isLetter(ch)){
+        //         currentString+=ch;
+        //     }else if(ch=='['){
+        //         countStack.push(currentNumber);
+        //         stringStack.push(currentString);
+        //         currentNumber=0;
+        //         currentString="";
+        //     }else if(ch==']'){
+        //         int repeat=countStack.pop();
+        //         String prev=stringStack.pop();
+        //         String repeated=currentString.repeat(repeat);
+        //         currentString=prev+repeated;
+        //     }
+        // }
+        // return currentString;
+        int[] index=new int[]{0};
+        return decode(s,index);
+    }
+
+    String decode(String s, int[] i){
+        StringBuilder currentString=new StringBuilder();
+        int num=0;
+        while(i[0]<s.length()&& s.charAt(i[0])!=']'){
+            char ch=s.charAt(i[0]);
             if(Character.isDigit(ch)){
-                currentNumber=currentNumber*10+(ch-'0');
+                num=num*10+(ch-'0');
+                i[0]++;
             }else if(Character.isLetter(ch)){
-                currentString+=ch;
+                currentString.append(ch);
+                i[0]++;
             }else if(ch=='['){
-                countStack.push(currentNumber);
-                stringStack.push(currentString);
-                currentNumber=0;
-                currentString="";
-            }else if(ch==']'){
-                int repeat=countStack.pop();
-                String prev=stringStack.pop();
-                String repeated=currentString.repeat(repeat);
-                currentString=prev+repeated;
+                i[0]++;
+                String inner=decode(s,i);
+                for(int j=0;j<num;j++){
+                    currentString.append(inner);
+                }
+                num=0;
             }
         }
-        return currentString;
+
+        i[0]++;
+        return currentString.toString();
     }
 }
