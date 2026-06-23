bc380237d132d29de83fef427a59465732ff1e30
0xlakhe <249052680+0xlakhe@users.noreply.github.com>
2026-06-23 18:44:50 +0545

    Time: 4 ms (69.01%), Space: 81.9 MB (87.51%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
@@ -15,28 +15,42 @@
  */
 class Solution {
     public int minDepth(TreeNode root) {
+        // if(root==null){
+        //     return 0;
+        // }
+        // Queue<TreeNode> queue= new LinkedList<>();
+        // queue.add(root);
+        // int depth=0;
+        // while(!queue.isEmpty()){
+        //     int size=queue.size();
+        //     depth+=1;
+        //     for(int i=0;i<size;i++){
+        //         TreeNode curr= queue.poll();
+        //         if(curr.left==null && curr.right==null){
+        //             return depth;
+        //         }
+        //         if(curr.left!=null){
+        //             queue.add(curr.left);
+        //         }
+        //         if(curr.right!=null){
+        //             queue.add(curr.right);
+        //         }
+        //     }
+        // }
+        // return depth;
         if(root==null){
             return 0;
         }
-        Queue<TreeNode> queue= new LinkedList<>();
-        queue.add(root);
-        int depth=0;
-        while(!queue.isEmpty()){
-            int size=queue.size();
-            depth+=1;
-            for(int i=0;i<size;i++){
-                TreeNode curr= queue.poll();
-                if(curr.left==null && curr.right==null){
-                    return depth;
-                }
-                if(curr.left!=null){
-                    queue.add(curr.left);
-                }
-                if(curr.right!=null){
-                    queue.add(curr.right);
-                }
-            }
+        if(root.left==null && root.right==null){
+            return 1;
         }
-        return depth;
+        if (root.left==null){
+            return minDepth(root.right)+1;
+        }else if(root.right==null){
+            return minDepth(root.left)+1;
+        }
+        int left= minDepth(root.left);
+        int right=minDepth(root.right);
+        return Math.min(left,right)+1;
     }
 }
