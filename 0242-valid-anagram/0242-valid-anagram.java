597d26018b9acd86d60e5b19774344a9f0818348
0xlakhe <249052680+0xlakhe@users.noreply.github.com>
2026-07-10 21:30:19 +0545

    Time: 5 ms (61.55%), Space: 44.2 MB (89.67%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -7,11 +7,10 @@ class Solution {
         int[] tMap=new int[26];
         for(int i=0;i<s.length();i++){
             char c=s.charAt(i);
+            char ch=t.charAt(i);
             sMap[c-'a']++;
-        }
-        for(int j=0;j<s.length();j++){
-            char ch=t.charAt(j);
             tMap[ch-'a']++;
+
         }
 
         for (int k=0;k<sMap.length;k++){
